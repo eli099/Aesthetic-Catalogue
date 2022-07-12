aafa06c02110b309360f968ad4dc17f506f9007d
Eliott McKenzie <101528348+eli099@users.noreply.github.com>
2022-07-12 23:26:57 +0100

    added .env file

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+# ! To create .env file
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1srncyes4^hw!g#4j-6wy*lwzsz!lue=77rh-6l5n$4tz=2v(_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +87,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aesthetics',
+        'NAME': env('DATABASE_NAME'),
         'HOST': 'localhost',
         'PORT': 5432
     }
